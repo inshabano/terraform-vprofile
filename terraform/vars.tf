f911da4bf5bfd838487e057735430fe6f6f0b5b8
Insha Bano <inshabano88158@gmail.com>
2024-06-02 20:22:44 +0530

    variable

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,11 +1,11 @@
 variable "REGION" {
-   description = "aws region"
-   type        = string
-   default     = "us-east-1"
+  description = "aws region"
+  type        = string
+  default     = "us-east-1"
 }
 
-variable "CLUSTER-NAME" {
-   description = "eks cluster name"
-   type        = string
-   default     = "gitops-eks"
+variable "CLUSTER_NAME" {
+  description = "eks cluster name"
+  type        = string
+  default     = "vprofile-eks"
 }
